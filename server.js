0399ce1553f64541177c7330c56823605f5a870b
Thuy.ttt <thuy09042001@gmail.com>
2022-04-26 14:10:51 +0700

    fix

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const {sequelize} = require('./models');
-const {rootRouter} = require('./routers');
-const { QueryTypes } = require('sequelize');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const { sequelize } = require("./models");
+const { rootRouter } = require("./routers");
+const { QueryTypes } = require("sequelize");
 const app = express();
 const port = 3000;
 
-app.use(express.json({
-    limit: '50mb'
-  }));
-  
-app.use(express.urlencoded({
-  limit: '50mb',
-  parameterLimit: 100000,
-  extended: true 
-}));
-  
-app.use("/public", express.static(path.join(__dirname, './public')));
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    parameterLimit: 100000,
+    extended: true,
+  })
+);
+
+app.use("/public", express.static(path.join(__dirname, "./public")));
 
 app.use(cors());
 
-app.use('/api', rootRouter);  
+app.use("/api", rootRouter);
 
 // app.get('/api/', (req, res) => {
 //   res.send('Hello World!')
 // })
 
-app.listen(port, async () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, async () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
   try {
     await sequelize.authenticate();
     console.log(`Connection has been established successfully 1`);
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
-}
+    console.error("Unable to connect to the database:", error);
+  }
 });
 
 // const connect = async () => {
