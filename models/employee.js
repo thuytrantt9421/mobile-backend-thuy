8d16104fd80e349bef54e915f82b9cc360e93bff
Thuy.ttt <thuy09042001@gmail.com>
2022-05-13 14:53:29 +0700

    fix api employee

--- models/employee.js
+++ models/employee.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Employee extends Model {
     /**
@@ -9,34 +7,37 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({User, Department, Position}) {
+    static associate({ User, Department, Position }) {
       // define association here
-      this.belongsTo(User, {foreignKey: 'user_id'});
-      this.belongsTo(Department, {foreignKey: 'department_id'});
-      this.belongsTo(Position, {foreignKey: 'position_id'});
+      this.belongsTo(User, { foreignKey: "user_id" });
+      this.belongsTo(Department, { foreignKey: "department_id" });
+      this.belongsTo(Position, { foreignKey: "position_id" });
     }
   }
-  Employee.init({
-    code: DataTypes.INTEGER,
-    name: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    DOB: DataTypes.DATE,
-    gender: DataTypes.STRING,
-    cccd: DataTypes.STRING,
-    front_photo: DataTypes.STRING,
-    back_photo: DataTypes.STRING,
-    timekeeping_photo: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    personalEmail: DataTypes.STRING,
-    companyEmail: DataTypes.STRING,
-    address: DataTypes.STRING,
-    skype: DataTypes.STRING,
-    facebook: DataTypes.STRING,
-    work_status: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Employee',
-  });
+  Employee.init(
+    {
+      // code: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      firstName: DataTypes.STRING,
+      DOB: DataTypes.DATE,
+      gender: DataTypes.STRING,
+      cccd: DataTypes.STRING,
+      front_photo: DataTypes.STRING,
+      back_photo: DataTypes.STRING,
+      timekeeping_photo: DataTypes.STRING,
+      phoneNumber: DataTypes.STRING,
+      personalEmail: DataTypes.STRING,
+      companyEmail: DataTypes.STRING,
+      address: DataTypes.STRING,
+      skype: DataTypes.STRING,
+      facebook: DataTypes.STRING,
+      work_status: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Employee",
+    }
+  );
   return Employee;
 };
