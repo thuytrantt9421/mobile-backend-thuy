2fa36db8194faa52d34b9610ff320ffacc542812
Thuy.ttt <thuy09042001@gmail.com>
2022-05-23 00:19:29 +0700

    add api

--- routers/donxinnghi.router.js
+++ routers/donxinnghi.router.js
@@ -1,14 +1,36 @@
-const express = require('express');
-const { userGetList, adminGetList, userCreateDonxinnghi, adminAcceptDonxinnghi, deleteDonxinnghi } = require('../controllers/donxinnghi.controller');
-const { authenticate } = require('../middleware/authenticate');
-const { authorize } = require('../middleware/authorize');
+const express = require("express");
+const {
+  userGetList,
+  adminGetList,
+  userCreateDonxinnghi,
+  adminAcceptDonxinnghi,
+  deleteDonxinnghi,
+  adminRefuseDonxinnghi,
+} = require("../controllers/donxinnghi.controller");
+const { authenticate } = require("../middleware/authenticate");
+const { authorize } = require("../middleware/authorize");
 
 const donxinnghiRouter = express.Router();
 
-donxinnghiRouter.get('/userGetList', authenticate, userGetList);
-donxinnghiRouter.get('/adminGetList', authenticate, authorize, adminGetList);
-donxinnghiRouter.post('/createDonxinnghi', authenticate, userCreateDonxinnghi);
-donxinnghiRouter.put('/acceptDonxinnghi/:id', authenticate, authorize, adminAcceptDonxinnghi);
-donxinnghiRouter.delete('/deleteDonxinnghi/:id', authenticate, deleteDonxinnghi)
+donxinnghiRouter.get("/userGetList", authenticate, userGetList);
+donxinnghiRouter.get("/adminGetList", authenticate, authorize, adminGetList);
+donxinnghiRouter.post("/createDonxinnghi", authenticate, userCreateDonxinnghi);
+donxinnghiRouter.put(
+  "/acceptDonxinnghi/:id",
+  authenticate,
+  authorize,
+  adminAcceptDonxinnghi
+);
+donxinnghiRouter.put(
+  "/refuseDonxinnghi/:id",
+  authenticate,
+  authorize,
+  adminRefuseDonxinnghi
+);
+donxinnghiRouter.delete(
+  "/deleteDonxinnghi/:id",
+  authenticate,
+  deleteDonxinnghi
+);
 
-module.exports = {donxinnghiRouter}
+module.exports = { donxinnghiRouter };
