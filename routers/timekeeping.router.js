b160773a2548a4e4afbb43ac0340b373e500ad91
Thuy.ttt <thuy09042001@gmail.com>
2022-05-22 09:03:44 +0700

    add api

--- routers/timekeeping.router.js
+++ routers/timekeeping.router.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const { timeKeeping, getTimeKeeping } = require('../controllers/timekeeping.controller');
-const { authenticate } = require('../middleware/authenticate');
+const express = require("express");
+const {
+  timeKeeping,
+  getTimeKeeping,
+  thongtinchamcong,
+} = require("../controllers/timekeeping.controller");
+const { authenticate } = require("../middleware/authenticate");
 
 const timeKeepingRouter = express.Router();
 
-timeKeepingRouter.post('/chamcong', authenticate,timeKeeping);
-timeKeepingRouter.post('/lichsu', authenticate, getTimeKeeping);
-module.exports = {timeKeepingRouter}
+timeKeepingRouter.post("/chamcong", authenticate, timeKeeping);
+timeKeepingRouter.post("/lichsu", authenticate, getTimeKeeping);
+timeKeepingRouter.get("/thongtinchamcong", authenticate, thongtinchamcong);
+module.exports = { timeKeepingRouter };
